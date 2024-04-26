eff3ad3f7c851633b695a5014374c7576cf53b6f
boy2balls <nguyendanghoaa@gmail.com>
2024-04-26 13:23:19 +0700

    wip

--- mail-service/src/main.ts
+++ mail-service/src/main.ts
@@ -1,29 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
     options: {
       host: process.env.HOST,
-      port: parseInt(process.env.PORT!),
-    },
-  });
+      port: parseInt(process.env.PORT!)
+    }
+  })
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.GRPC,
     options: {
       url: `${process.env.HOST}:${process.env.GRPC_PORT}`,
       package: 'user',
-      protoPath: '../_proto/user.proto',
-    },
-  });
-  await app.startAllMicroservices();
-  await app.listen(process.env.PORT!);
+      protoPath: '../_proto/user.proto'
+    }
+  })
+  await app.startAllMicroservices()
+  await app.listen(process.env.PORT!)
 }
 
 bootstrap().then(() => {
-  console.log(`Application is running on: ${process.env.PORT}`);
-});
+  console.log(`Application is running on: ${process.env.PORT}`)
+})
--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -4,18 +4,25 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 import * as process from 'process'
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-        host: process.env.HOST,
-        port: parseInt(process.env.PORT!)
-      }
+  const app = await NestFactory.create(AppModule)
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: process.env.HOST,
+      port: parseInt(process.env.PORT!)
     }
-  )
+  })
 
-  await app.listen()
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      url: `${process.env.HOST}:${process.env.GRPC_PORT}`,
+      package: 'user',
+      protoPath: '../_proto/user.proto'
+    }
+  })
+  await app.startAllMicroservices()
+  await app.listen(process.env.PORT!)
 }
 
 bootstrap().then(() => {
